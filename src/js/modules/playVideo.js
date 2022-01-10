20520ca14ad71a6dc530da7073fc5750e5956f73
AlexHorovyi <alexhorovyi@gmail.com>
2022-01-10 23:20:08 +0200

    video fixed bug

--- src/js/modules/playVideo.js
+++ src/js/modules/playVideo.js
@@ -1,13 +1,13 @@
 export default class VideoPlayer {
-    constructor(triggers, overlay, ) {
+    constructor(triggers, overlay) {
         this.btns = document.querySelectorAll(triggers);
         this.overlay = document.querySelector(overlay);
         this.close = this.overlay.querySelector('.close');
     }
+
     bindTriggers() {
         this.btns.forEach(btn => {
             btn.addEventListener('click', () => {
-
                 if (document.querySelector('iframe#frame')) {
                     this.overlay.style.display = 'flex';
                 } else {
@@ -18,18 +18,22 @@ export default class VideoPlayer {
             });
         });
     }
+
     bindCloseBtn() {
         this.close.addEventListener('click', () => {
             this.overlay.style.display = 'none';
             this.player.stopVideo();
         });
     }
+
     createPlayer(url) {
         this.player = new YT.Player('frame', {
             height: '100%',
             width: '100%',
             videoId: `${url}`
         });
+
+        console.log(this.player);
         this.overlay.style.display = 'flex';
     }
 
