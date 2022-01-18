f9eaa9a1f964047012e0bdf77a885a4bcc666395
D:\Vitalik\WORK\vcpkg <git clone https://github.com/Microsoft/vcpkg.git cd vcpkg ./bootstrap-vcpkg.sh ./vcpkg integrate install vcpkg install curl[tool]>
2022-01-18 18:21:28 +0200

    slider-mini

---
+++ src/js/modules/slider/slider-mini.js
@@ -0,0 +1,77 @@
+import Slider from './slider';
+
+export default class MiniSlider extends Slider {
+    constructor(container, next, prev, activeClass, animate, autoplay) {
+        super(container, next, prev, activeClass, animate, autoplay);
+    }
+
+    decorizeSlide() {
+        this.slides.forEach(slide => {
+            slide.classList.remove(this.activeClass);
+            if (this.animate) {
+                slide.querySelector('.card__title').style.opacity = '0.4';
+                slide.querySelector('.card__controls-arrow').style.opacity = '0';
+            }
+
+        });
+
+        if (!this.slides[0].closest("button")) {
+            this.slides[0].classList.add(this.activeClass);
+        }
+
+        if (this.animate) {
+            this.slides[0].querySelector('.card__title').style.opacity = '1';
+            this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';
+        }
+    }
+
+    nextSlide() {
+        if (this.slides[1].tagName == "BUTTON" && this.slides[2].tagName == "BUTTON") {
+            this.container.appendChild(this.slides[0]); // Slide
+            this.container.appendChild(this.slides[1]); // btn
+            this.container.appendChild(this.slides[2]); // btn
+            this.decorizeSlide();
+        } else if (this.slides[1].tagName == "BUTTON") {
+            this.container.appendChild(this.slides[0]); // Slide
+            this.container.appendChild(this.slides[1]);
+            this.decorizeSlide();
+        } else {
+            this.container.appendChild(this.slides[0]);
+            this.decorizeSlide();
+        }
+    }
+
+    bindTriggers() {
+        this.next.addEventListener('click', () => this.nextSlide());
+
+        this.prev.addEventListener('click', () => {
+
+            for (let i = this.slides.lenght - 1; i > 0; i--) {
+                if (this.slides[1].tagName !== "BUTTON") {
+                    let active = this.slides[i];
+
+                    this.container.insertBefore(active, this.slides[0]);
+                    this.decorizeSlide();
+                    break;
+                }
+            }
+
+
+        });
+    }
+
+    init() {
+        this.container.style.cssText = `
+        display: flex;
+        flex-wrap: wrap;
+        overflow: hidden;
+        align-items: flex-start;
+        `;
+        this.bindTriggers();
+        this.decorizeSlide();
+
+        if (this.autoplay) {
+            setInterval(() => this.nextSlide(), 5000);
+        }
+    }
+}
